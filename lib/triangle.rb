3418b4cd45667a12eca2da117bbaf2317ab66849
Srikalyan Megati <kalmegati@gmail.com>
2019-10-11 13:36:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,19 @@
 class Triangle
   
-  def initialize()
+  def initialize(ein, zwei, drei)
+    
+    
+    
+  end
+  
+  def kind
+    if ein == zwei && zwei == drei
+      :equilateral
+    elsif ein == zwei || zwei == drei
+      :isosceles
+    else
+      :scalene
+    end
+  end
   
 end
